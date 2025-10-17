51b09a3fe2a8a76b9de1092cc2a93740740a6bd2
Josiel Maicon <josielmaicon.a@gmail.com>
2025-10-16 22:10:58 -0300

    layout de compra ajustado

--- interface/src/layouts/PontoVendaLayout.jsx
+++ interface/src/layouts/PontoVendaLayout.jsx
@@ -1,34 +1,49 @@
 // src/layouts/ComprasPageLayout.jsx
 
 export default function ComprasPageLayout({
-  Header,
-  SidePanelTop,
-  SidePanelBottom,
+  Header1,
+  Header2,
+  SidePanel,
   MainContent,
   Footer,
+  Resume
 }) {
   return (
-    <div className="flex-1 p-2 pt-4 flex flex-col h-full">
+    <div className="font-mono flex-1 p-2 pt-4 flex flex-col h-full bg-muted">
       <div className="grid flex-grow grid-rows-[0.2fr_1fr_0.2fr] gap-2 h-full min-h-0">
-        <div className="bg-card rounded-xl border p-4 flex items-center justify-center">
-          {Header || "[CABEÇALHO]"}
-        </div>
-        <div className="grid grid-cols-[  4fr_6fr] gap-2 min-h-0">
-          <div className="grid grid-rows-2 gap-2 min-h-0">
-            <div className="bg-card rounded-xl border p-4 flex items-center justify-center">
-              {SidePanelTop || "[PAINEL LATERAL - TOPO]"}
-            </div>
-            <div className="bg-card rounded-xl border p-4 flex items-center justify-center">
-              {SidePanelBottom || "[PAINEL LATERAL - BAIXO]"}
-            </div>
+        <div className="grid flex-grow grid-cols-[4fr_6fr] gap-2 h-full min-h-0">
+          <div className="bg-card p-4 flex items-center justify-center">
+            {Header1 || "[CABEÇALHO 1]"}
           </div>
-          <div className="bg-card rounded-xl border p-4 flex items-center justify-center">
-            {MainContent || "[CONTEÚDO PRINCIPAL]"}
+          <div className="bg-card  p-4 flex items-center justify-center">
+            {Header2 || "[CABEÇALHO 2]"}
           </div>
         </div>
-        <div className="bg-card rounded-xl border p-4 flex items-center justify-center">
+
+        <div className="grid grid-cols-[4fr_6fr] gap-2 min-h-0">
+          
+          <div className="grid grid-rows-1 gap-2 min-h-0">
+            <div className="bg-card p-4 flex items-center justify-center">
+              {SidePanel || "[PAINEL LATERAL]"}
+            </div>
+          </div>
+
+          <div className="grid grid-rows-[6fr_1fr] gap-2 min-h-0">
+            <div className="bg-card p-4 flex items-center justify-center">
+              {MainContent || "[CONTEÚDO PRINCIPAL]"}
+            </div>
+            <div className="bg-card p-4 flex items-center justify-center">
+              {Resume || "[SUBTOTAL]"}
+            </div>
+          </div>
+
+          
+        </div>
+
+        <div className="bg-card p-4 flex items-center justify-center">
           {Footer || "[RODAPÉ]"}
         </div>
+
       </div>
     </div>
   );
